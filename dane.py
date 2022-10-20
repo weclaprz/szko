e8244e15ca147b724bd12ef102f4ec6da048300f
WeclaPrz <weclasprzemyslaw@gmail.com>
2022-10-20 15:43:20 +0200

    Zmiana 2

--- dane.py
+++ dane.py
@@ -27,7 +27,7 @@ coToZaTyp = coToZaTyp + (1,2,3,4,5,6,7,8,9,0,1,2,3,4,5)
 # print(lista)
 
 
-numery =[1,2,3]
-cyfry = numery
-numery =[11,22,33]
-print(cyfry)
+# numery =[1,2,3]
+# cyfry = numery
+# numery =[11,22,33]
+# print(cyfry)
---
+++ zbior.py
@@ -0,0 +1,11 @@
+zbior1 = {1,2,3,4,5,4,3,6,9}
+print(zbior1)
+
+lista = [3, 3, 3, 6, 4, 5, 1, 1, 2]
+print(lista)
+zbior5 = set(lista)
+print(zbior5)
+lista = list(zbior5)
+print(lista)
+krotka = tuple(lista)
+print(krotka)
