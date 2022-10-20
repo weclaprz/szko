ebaaa9a860d1f5a7195c32beb7c39e6ff5d92325
WeclaPrz <weclasprzemyslaw@gmail.com>
2022-10-20 15:54:21 +0200

    Zmiana 15:54

---
+++ slownik.py
@@ -0,0 +1,13 @@
+slownik = {1: "Tomek",
+           "wiek": 39,
+           "hobby": ['programowanie', 'jedzenie']}
+
+
+print(slownik.keys())
+print(slownik['hobby'][0])
+print(slownik.values())
+print(slownik.items())
+slownik["wiek"] = 40
+slownik["znakZ"] = "Ryby"
+slownik.pop('wiek')
+print(slownik)
