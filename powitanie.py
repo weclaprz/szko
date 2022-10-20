61baae76ce1783b6d10954851ae9014b89069fb3
Węcłaś Przemysław - Korpo <Przemyslaw.Weclas@orange.com>
2022-10-20 13:09:49 +0200

    Initial commit

---
+++ powitanie.py
@@ -0,0 +1,20 @@
+
+
+print("Test")
+print("Test")
+print("Test")
+print("Test")
+print('''
+**************************************************
+*                                                *
+*                Witam w progamie                *
+*                                                *
+**************************************************
+''')
+#TODO: test scjsh
+# sdsddsdsdjk
+# dsdjkds
+
+#Koidkskjdkjj
+zm = input("Podaj cos : ")
+print(zm)
